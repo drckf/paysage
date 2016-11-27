09c98535e7f19d0cdf8a837cff237e716a7d8e1e
Charles Fisher <charlesfisher@Charless-MacBook-Pro.local>
2016-11-27 07:33:16 -0800

    readme

---
+++ paysage/README.rst
@@ -0,0 +1,10 @@
+Paysage
+- - - -
+
+To install: 
+1. Clone the git repo.
+2. CD into the directory with setup.py
+3. Run “pip install -e .”
+
+About the name:
+The name refers to “paysage d’energy,” which means energy landscape in French.  
--- paysage/setup.py
+++ paysage/setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='paysage',
       version='0.0',
       description='Machine learning with energy based models in python',
@@ -8,4 +12,9 @@ setup(name='paysage',
       author_email='charleskennethfisher@gmail.com',
       license='MIT',
       packages=['paysage'],
+      install_requires=[
+                        'numpy',
+                        'pandas',
+                        'h5py'
+                        ],
       zip_safe=False)
