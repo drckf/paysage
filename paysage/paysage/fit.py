629da2d23152f85cf39fab2fc828afe925d6ca95
Charles Fisher <charlesfisher@Charless-MacBook-Pro.local>
2016-11-27 07:57:49 -0800

    template function for training

--- paysage/paysage/fit.py
+++ paysage/paysage/fit.py
@@ -56,6 +56,57 @@ class SequentialMC(object):
             new_state[i,:] = self.state[indices[i]]
         self.state = new_state
         
+
+def basic_train(model, batch, epochs, method="RMSprop", verbose=True):
+    momentum = 0.0
+    sampler = fit.SequentialMC.from_batch(model, batch)
+    steps = {key: 0.001 for key in model.params}
+    grad = {key: numpy.zeros_like(model.params[key]) for key in model.params}
+    delta = {key: numpy.zeros_like(model.params[key]) for key in model.params}
+    mean_square = {key: numpy.zeros_like(model.params[key]) for key in model.params}
+
+    # an array to store the reconstruction error values during the descent
+    mem = []
+        
+    for epoch in range(epochs):
+        #learning rate decays slowly duing descent
+        lr = 0.8**epoch
+        
+        t = 0
+        while True:
+            # grab a minibatch from the observed data
+            try:
+                v_data = batch.get()
+            except StopIteration:
+                break
+            
+            # generate a sample from the model
+            sampler = fit.SequentialMC(model, v_data) 
+            sampler.update_state(steps)    
+            sampler.resample_state(temperature=1.0)
+            v_model = sampler.state
+            
+            # compute the gradients
+            grad = {key: gradient(model, key, v_data, v_model) for key in model.params}
+
+            # compute the updates using RMSprop (slide 29 lecture 6 of Geoff Hinton's coursera course) or momentum
+            if method == "RMSprop":
+                for key in model.params:
+                    mean_square[key][:] = 0.9 * mean_square[key] + (1.0 - 0.9) * grad[key]**2
+                    delta[key][:] = (lr * steps[key]) / (0.000001 + numpy.sqrt(mean_square[key])) * grad[key]
+            elif method == "momentum":
+                for key in model.params:
+                    delta[key][:] = lr * steps[key] * grad[key] + momentum * delta[key] 
+            else:
+                raise ValueError("method must be one of: RMSprop, momentum")
+                
+            # update the parameters
+            for key in model.params:
+                model.params[key][:] = model.params[key] + delta[key]
+        
+    return (a, b, W, mem)
+
+        
         
 """
 Steps in training algorithm:
@@ -128,63 +179,3 @@ class StochasticGradientDescent(object):
         assert method.lower() in ['sgd', 'momentum', 'rmsprop']
         self.method = method.lower()
         
-"""
-    
-# descent: (array, int, tuple, int, int, float, int, OPTIONAL) -> tuple
-def descent(reader, n_hidden, n, momentum, epochs, method = "RMSprop", verbose = True):
-    astep, bstep, Wstep = 0.001, 0.001, 0.001
-    a,b,W = random_params(reader.rows * reader.cols, n_hidden, (-0.5, -0.2, 0.0), (0.05, 0.05, 0.5))
-    Da, Db, DW = numpy.zeros_like(a), numpy.zeros_like(b), numpy.zeros_like(W) 
-    MSa, MSb, MSW = numpy.zeros_like(a), numpy.zeros_like(b), numpy.zeros_like(W) 
-    # an array to store the reconstruction error values during the descent
-    mem = []
-        
-    for epoch in range(epochs):
-        #learning rate decays slowly duing descent
-        lr = 0.8**epoch
-        final_batch = False
-        
-        t = 0
-        while not final_batch:
-            v_data, labels, final_batch = reader.yield_next_minibatch()
-            
-            if epoch == 0:
-                h_data, v_free, h_free = gibbs(v_data, a, b, W, n)
-            else:
-                h_data, v_free, h_free = gibbs(v_free, a, b, W, n)
-            if t % 100 == 0:
-                err = rmse(v_data, v_free)
-                if verbose:
-                    print("{0}: {1}: {2:.4f}".format(epoch,t,err))
-                mem.append(err)      
-            t += 1
-            
-            # compute the gradients
-            da, db, dW = grad_a(v_data, v_free), grad_b(h_data, h_free), grad_W(h_data, v_data, h_free, v_free)
-
-            # compute the updates using RMSprop (slide 29 lecture 6 of Geoff Hinton's coursera course) or momentum
-            if method == "RMSprop":
-                MSa = 0.9*MSa + (1-0.9)*da**2
-                MSb = 0.9*MSb + (1-0.9)*db**2
-                MSW = 0.9*MSW + (1-0.9)*dW**2
-                
-                Da = lr*astep*da / (0.00001 + numpy.sqrt(MSa))   
-                Db = lr*bstep*db / (0.00001 + numpy.sqrt(MSb))  
-                DW = lr*Wstep*dW / (0.00001 + numpy.sqrt(MSW))
-            elif method == "momentum":
-                Da = lr*astep*da + momentum*Da   
-                Db = lr*bstep*db + momentum*Db     
-                DW = lr*Wstep*dW + momentum*DW    
-            else:
-                raise ValueError("method must be one of: RMSprop, momentum")
-                                
-            # update the paramters
-            #a, W = a + Da, W + DW
-            a, b, W = a + Da, b + Db, W + DW
-        
-    
-    return (a, b, W, mem)
-
-if __name__ == "__main__":
-    pass
-"""
