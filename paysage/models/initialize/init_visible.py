0a76ef52af360547b692b14e1999ca821f2cfa81
Charles Fisher <charlesfisher@Charless-MacBook-Pro.local>
2017-02-07 21:17:16 -0800

    fix mean_field fields

--- paysage/models/initialize/init_visible.py
+++ paysage/models/initialize/init_visible.py
@@ -48,8 +48,35 @@ def mean_field(batch, model):
         x2 += B.dot(v_data.T, v_data)
         nsamples += len(v_data)
 
-    model.params['visible_bias'] = model.layers['visible'].inverse_mean(x/nsamples)
     cov = x2/nsamples - (x/nsamples)**2
     J = -numpy.linalg.pinv(cov)
     numpy.fill_diagonal(J, 0)
     model.params['weights'] = J
+    model.params['visible_bias'] = model.layers['visible'].inverse_mean(x/nsamples)
+    model.params['visible_bias'] -= B.dot(J, x/nsamples)
+
+def jacquin_rancon(batch, model):
+    """
+    Jacquin, Hugo, and A. Rançon.
+    "Resummed mean-field inference for strongly coupled data."
+    Physical Review E 94.4 (2016): 042118.
+    """
+    nvis = len(model.params['visible_bias'])
+    x = numpy.zeros(nvis, dtype=numpy.float32)
+    x2 = numpy.zeros((nvis,nvis), dtype=numpy.float32)
+    nsamples = 0
+    while True:
+        try:
+            v_data = batch.get(mode='train')
+        except StopIteration:
+            break
+        x += B.msum(v_data, axis=0)
+        x2 += B.dot(v_data.T, v_data)
+        nsamples += len(v_data)
+
+    cov = x2/nsamples - (x/nsamples)**2
+    J = -numpy.linalg.pinv(cov)
+    numpy.fill_diagonal(J, 0)
+    model.params['weights'] = J
+    model.params['visible_bias'] = model.layers['visible'].inverse_mean(x/nsamples)
+    model.params['visible_bias'] -= B.dot(J, x/nsamples)
