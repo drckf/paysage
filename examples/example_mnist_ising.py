64acc4aad2a07f8210b63579141b83386a15f526
Charles Fisher <charlesfisher@Charless-MacBook-Pro.local>
2017-02-11 08:08:07 -0800

    clean up ising model

--- examples/example_mnist_ising.py
+++ examples/example_mnist_ising.py
@@ -1,9 +1,8 @@
-import os, sys, numpy, pandas, time
+import os, sys, numpy, pandas
 
 from paysage import batch
 from paysage.models import visible
 from paysage import fit
-from paysage import optimizers
 
 try:
     import plotting
@@ -13,9 +12,6 @@ except ImportError:
 if __name__ == "__main__":
 
     batch_size = 50
-    num_epochs = 10
-    learning_rate = 0.001
-    mc_steps = 1
 
     paysage_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     filepath = os.path.join(paysage_path, 'mnist', 'mnist.h5')
@@ -37,62 +33,28 @@ if __name__ == "__main__":
                        transform=batch.color_to_ising,
                        train_fraction=0.99)
 
-    # set up the model and initialize the parameters
-    rbm = visible.IsingModel(data.ncols)
-    rbm.initialize(data, method='tap')
+    # set up the model
+    ising = visible.IsingModel(data.ncols)
 
-    # set up the optimizer and the fit method
-    opt = optimizers.ADAM(rbm,
-                          stepsize=learning_rate,
-                          scheduler=optimizers.PowerLawDecay(0.1))
-
-    sampler = fit.DrivenSequentialMC.from_batch(rbm, data,
-                                                method='stochastic')
-
-    cd = fit.PCD(rbm,
-                 data,
-                 opt,
-                 sampler,
-                 num_epochs,
-                 mcsteps=mc_steps,
-                 skip=200,
-                 metrics=['EnergyDistance',
-                          'EnergyGap',
-                          'EnergyZscore'])
-
-    # fit the model
-    print('training with contrastive divergence')
-    #cd.train()
+    # use the thouless-anderson-palmer (tap) approximation to estimate params
+    ising.initialize(data, method='tap')
 
     # evaluate the model
-    # this will be the same as the final epoch results
-    # it is repeated here to be consistent with the sklearn rbm example
     performance = fit.ProgressMonitor(0,
                                       data,
-                                      metrics=['ReconstructionError',
-                                               'EnergyDistance',
+                                      metrics=['EnergyDistance',
                                                'EnergyGap',
                                                'EnergyZscore'])
     print('Final performance metrics:')
-    performance.check_progress(rbm, 0, show=True)
-
-    print("\nPlot a random sample of reconstructions")
-    v_data = data.get('validate')
-    sampler = fit.DrivenSequentialMC(rbm)
-    sampler.initialize(v_data)
-    sampler.update_state(1)
-    v_model = rbm.deterministic_step(sampler.state)
-
-    idx = numpy.random.choice(range(len(v_model)), 5, replace=False)
-    grid = numpy.array([[v_data[i], v_model[i]] for i in idx])
-    plotting.plot_image_grid(grid, (28,28), vmin=grid.min(), vmax=grid.max())
+    performance.check_progress(ising, 0, show=True)
 
     print("\nPlot a random sample of fantasy particles")
-    random_samples = rbm.random(v_data)
-    sampler = fit.DrivenSequentialMC(rbm)
+    v_data = data.get('validate')
+    random_samples = ising.random(v_data)
+    sampler = fit.DrivenSequentialMC(ising)
     sampler.initialize(random_samples)
     sampler.update_state(1000)
-    v_model = rbm.deterministic_step(sampler.state)
+    v_model = ising.deterministic_step(sampler.state)
 
     idx = numpy.random.choice(range(len(v_model)), 5, replace=False)
     grid = numpy.array([[v_model[i]] for i in idx])
