87fd499fc326b9a0dbca5ed9ac58b90387ed6d87
Charles Fisher <charlesfisher@Charless-MacBook-Pro.local>
2017-02-11 09:20:03 -0800

    initial commit

--- paysage/backends/__init__.py
+++ paysage/backends/__init__.py
@@ -1,2 +1,16 @@
-from .nonlinearity import *
-from .matrix import *
+import json, os
+
+filedir = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(filedir,"config.json"), "r") as infile:
+    config = json.load(infile)
+
+if config['backend'] == 'python':
+    from .python_backend.matrix import *
+    from .python_backend.nonlinearity import *
+elif config['backend'] == 'pytorch':
+    from .pytorch_backend.matrix import *
+    from .pytorch_backend.nonlinearity import *
+else:
+    raise ValueError(
+    "Unknown backend {}".format(config['backend'])
+    )
