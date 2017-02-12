a23e00be38526b56103eaefbbfc6869bda90f0ab
Charles Fisher <charlesfisher@Charless-MacBook-Pro.local>
2017-02-11 16:47:11 -0800

    continuing refactor

--- paysage/backends/__init__.py
+++ paysage/backends/__init__.py
@@ -7,9 +7,11 @@ with open(os.path.join(filedir,"config.json"), "r") as infile:
 if config['backend'] == 'python':
     from .python_backend.matrix import *
     from .python_backend.nonlinearity import *
+    from .python_backend.random import *
 elif config['backend'] == 'pytorch':
     from .pytorch_backend.matrix import *
     from .pytorch_backend.nonlinearity import *
+    from .pytorch_backend.random import *
 else:
     raise ValueError(
     "Unknown backend {}".format(config['backend'])
