fed12ef927c29fc97b53b23c8efb0806a2510ead
Charles Fisher <charleskennethfisher@gmail.com>
2017-03-06 19:01:40 -0800

    add rtol and atol to python allclose

--- paysage/backends/python_backend/matrix.py
+++ paysage/backends/python_backend/matrix.py
@@ -272,12 +272,12 @@ def equal(x, y):
     """
     return numpy.equal(x, y)
 
-def allclose(x, y):
+def allclose(x, y, rtol=1e-05, atol=1e-08):
     """
     Test if all elements in the two tensors are approximately equal.
 
     """
-    return numpy.allclose(x, y)
+    return numpy.allclose(x, y, rtol=1e-05, atol=1e-08)
 
 def not_equal(x, y):
     """
