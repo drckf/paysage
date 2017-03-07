92128d78530d71024be8b521ff29a91c2e867929
Charles Fisher <charleskennethfisher@gmail.com>
2017-03-07 06:39:50 -0800

    test logcosh

--- paysage/backends/backend_tests.py
+++ paysage/backends/backend_tests.py
@@ -271,6 +271,24 @@ def test_logaddexp():
     assert torch_matrix.allclose(torch_y, torch_py_y), \
     "torch -> python -> torch failure: cosh"
 
+def test_logcosh():
+    shape = (100, 100)
+    py_rand.set_seed()
+    py_x = py_rand.randn(shape)
+    torch_x = torch_matrix.float_tensor(py_x)
+
+    py_y = py_func.logcosh(py_x)
+    torch_y = torch_func.logcosh(torch_x)
+
+    torch_py_y = torch_matrix.float_tensor(py_y)
+    py_torch_y = torch_matrix.to_numpy_array(torch_y)
+
+    assert py_matrix.allclose(py_y, py_torch_y), \
+    "python -> torch -> python failure: logcosh"
+
+    assert torch_matrix.allclose(torch_y, torch_py_y), \
+    "torch -> python -> torch failure: logcosh"
+
 
 if __name__ == "__main__":
     test_conversion()
@@ -286,4 +304,5 @@ if __name__ == "__main__":
     test_tpow()
     test_cosh()
     test_logaddexp()
+    test_logcosh()
     test_tabs()
