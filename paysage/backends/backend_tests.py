c4434235ef2bab02c276b500178792d59939033d
Charles Fisher <charleskennethfisher@gmail.com>
2017-03-07 06:41:46 -0800

    reorder tests

--- paysage/backends/backend_tests.py
+++ paysage/backends/backend_tests.py
@@ -31,24 +31,6 @@ def test_conversion():
     assert torch_matrix.allclose(torch_y, torch_py_y), \
     "torch -> python -> torch failure"
 
-def test_acosh():
-    shape = (100, 100)
-    py_rand.set_seed()
-    py_x = 1 + py_rand.rand(shape)
-    torch_x = torch_matrix.float_tensor(py_x)
-
-    py_y = py_func.acosh(py_x)
-    torch_y = torch_func.acosh(torch_x)
-
-    torch_py_y = torch_matrix.float_tensor(py_y)
-    py_torch_y = torch_matrix.to_numpy_array(torch_y)
-
-    assert py_matrix.allclose(py_y, py_torch_y), \
-    "python -> torch -> python failure: acosh"
-
-    assert torch_matrix.allclose(torch_y, torch_py_y), \
-    "torch -> python -> torch failure: acosh"
-
 def test_tabs():
     shape = (100, 100)
     py_rand.set_seed()
@@ -289,10 +271,28 @@ def test_logcosh():
     assert torch_matrix.allclose(torch_y, torch_py_y), \
     "torch -> python -> torch failure: logcosh"
 
+def test_acosh():
+    shape = (100, 100)
+    py_rand.set_seed()
+    py_x = 1 + py_rand.rand(shape)
+    torch_x = torch_matrix.float_tensor(py_x)
+
+    py_y = py_func.acosh(py_x)
+    torch_y = torch_func.acosh(torch_x)
+
+    torch_py_y = torch_matrix.float_tensor(py_y)
+    py_torch_y = torch_matrix.to_numpy_array(torch_y)
+
+    assert py_matrix.allclose(py_y, py_torch_y), \
+    "python -> torch -> python failure: acosh"
+
+    assert torch_matrix.allclose(torch_y, torch_py_y), \
+    "torch -> python -> torch failure: acosh"
+
 
 if __name__ == "__main__":
     test_conversion()
-    test_acosh()
+    test_tabs()
     test_exp()
     test_log()
     test_tanh()
@@ -305,4 +305,5 @@ if __name__ == "__main__":
     test_cosh()
     test_logaddexp()
     test_logcosh()
-    test_tabs()
+    test_acosh()
+
