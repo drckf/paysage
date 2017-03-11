6bff3eaf2da49ba98ab28cb3f334f2b3b2023714
Charles Fisher <charleskennethfisher@gmail.com>
2017-03-11 10:29:32 -0800

    deal with pytorch instal directly in setup.py

--- setup.py
+++ setup.py
@@ -23,10 +23,24 @@ setup(name='paysage',
           'scikit-learn',
           'scipy',
           'seaborn',
-          'tables',
-          'torch'
-      ],
+          'tables'
+          ],
       tests_require=[
           'pytest'
       ],
       zip_safe=False)
+
+# PyTorch is not available through PyPi yet (but it is available through conda)
+# And, it is not available for windows
+# Need to deal with the dependencies directly
+import pip
+from sys import platform as _platform
+
+if _platform == "linux" or _platform == "linux2":
+    pip.main(['install', "https://s3.amazonaws.com/pytorch/whl/cu75/torch-0.1.10.post2-cp35-cp35m-linux_x86_64.whl"])
+    pip.main(['install', "torchvision"])
+elif _platform == "darwin":
+    pip.main(['install', "https://s3.amazonaws.com/pytorch/whl/torch-0.1.10.post1-cp35-cp35m-macosx_10_6_x86_64.whl "])
+    pip.main(['install', "torchvision"])
+elif _platform == "win32":
+   raise RuntimeError("PyTorch is not available for windows")
