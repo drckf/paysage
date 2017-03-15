128b3c79aa80e3b5a987d9c56e2af112803403eb
Charles Fisher <charleskennethfisher@gmail.com>
2017-03-15 09:41:57 -0700

    update __init__.py to remove visible models

--- paysage/__init__.py
+++ paysage/__init__.py
@@ -5,10 +5,8 @@ from . import fit
 from . import optimizers
 from . import metrics
 from .models.initialize import init_hidden
-from .models.initialize import init_visible
 from .models.initialize import init_recurrent
 from .models.initialize import init_deep
 from .models import hidden
-from .models import visible
 from .models import recurrent
 from .models import deep
