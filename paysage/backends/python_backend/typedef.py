88dce15c6bd1fac062c0fecf4512f327a049e7e7
Charles Fisher <charleskennethfisher@gmail.com>
2017-03-20 14:24:44 -0700

    typedef, python rand

--- paysage/backends/python_backend/typedef.py
+++ paysage/backends/python_backend/typedef.py
@@ -5,8 +5,15 @@ from numpy import float32
 Scalar = Union[int, float]
 
 FloatingPoint = Union[float32, ndarray]
-# aasuming here that ndarray.dtype == float32
+# assuming here that ndarray.dtype == float32
+# no good way to specify this right now
+
+Boolean = Union[bool, ndarray]
+# assuming here that ndarray.dtype == bool
 # no good way to specify this right now
 
 Tensor = ndarray
+
+NumpyTensor = ndarray
+
 Numeric = Union[Scalar, Tensor]
