92f2a34d154f16ae6b6e1a88a596d4592c8b7503
Charles Fisher <charleskennethfisher@gmail.com>
2017-03-21 10:45:53 -0700

    docstrings in python backend nonlinearity

--- paysage/backends/python_backend/nonlinearity.py
+++ paysage/backends/python_backend/nonlinearity.py
@@ -9,12 +9,24 @@ def tabs(x: T.Tensor) -> T.Tensor:
     """
     Elementwise absolute value of a tensor.
 
+    Args:
+        x: A tensor.
+
+    Returns:
+        tensor (non-negative): Absolute value of x.
+
     """
     return ne.evaluate('abs(x)')
 
 def exp(x: T.Tensor) -> T.Tensor:
     """
     Elementwise exponential function of a tensor.
+
+    Args:
+        x: A tensor.
+
+    Returns:
+        tensor (non-negative): Elementwise exponential.
 
     """
     return ne.evaluate('exp(x)')
@@ -23,12 +35,24 @@ def log(x: T.Tensor) -> T.Tensor:
     """
     Elementwise natural logarithm of a tensor.
 
+    Args:
+        x (non-negative): A tensor.
+
+    Returns:
+        tensor: Elementwise natural logarithm.
+
     """
     return ne.evaluate('log(x)')
 
 def tanh(x: T.Tensor) -> T.Tensor:
     """
     Elementwise hyperbolic tangent of a tensor.
+
+    Args:
+        x: A tensor.
+
+    Returns:
+        tensor: Elementwise hyperbolic tangent.
 
     """
     return ne.evaluate('tanh(x)')
@@ -37,6 +61,12 @@ def expit(x: T.Tensor) -> T.Tensor:
     """
     Elementwise expit (a.k.a. logistic) function of a tensor.
 
+    Args:
+        x: A tensor.
+
+    Returns:
+        tensor: Elementwise expit (a.k.a. logistic).
+
     """
     return ne.evaluate('(1 + tanh(x/2))/2')
 
@@ -44,12 +74,24 @@ def reciprocal(x: T.Tensor) -> T.Tensor:
     """
     Elementwise inverse of a tensor.
 
+    Args:
+        x (non-zero): A tensor:
+
+    Returns:
+        tensor: Elementwise inverse.
+
     """
     return numpy.reciprocal(x)
 
 def atanh(x: T.Tensor) -> T.Tensor:
     """
     Elementwise inverse hyperbolic tangent of a tensor.
+
+    Args:
+        x (between -1 and +1): A tensor.
+
+    Returns:
+        tensor: Elementwise inverse hyperbolic tangent
 
     """
     y = numpy.clip(x, a_min=EPSILON-1, a_max=1-EPSILON)
@@ -59,12 +101,24 @@ def sqrt(x: T.Tensor) -> T.Tensor:
     """
     Elementwise square root of a tensor.
 
+    Args:
+        x (non-negative): A tensor.
+
+    Returns:
+        tensor(non-negative): Elementwise square root.
+
     """
     return ne.evaluate('sqrt(x)')
 
 def square(x: T.Tensor) -> T.Tensor:
     """
     Elementwise square of a tensor.
+
+    Args:
+        x: A tensor.
+
+    Returns:
+        tensor (non-negative): Elementwise square.
 
     """
     return ne.evaluate('x**2')
@@ -73,12 +127,25 @@ def tpow(x: T.Tensor, a: T.Scalar) -> T.Tensor:
     """
     Elementwise power of a tensor x to power a.
 
+    Args:
+        x: A tensor.
+        a: Power.
+
+    Returns:
+        tensor: Elementwise x to the power of a.
+
     """
     return ne.evaluate('x**a')
 
 def cosh(x: T.Tensor) -> T.Tensor:
     """
     Elementwise hyperbolic cosine of a tensor.
+
+    Args:
+        x: A tensor.
+
+    Returns:
+        tensor: Elementwise hyperbolic cosine.
 
     """
     return ne.evaluate('cosh(x)')
@@ -87,6 +154,13 @@ def logaddexp(x1: T.Tensor, x2: T.Tensor) -> T.Tensor:
     """
     Elementwise logaddexp function: log(exp(x1) + exp(x2))
 
+    Args:
+        x1: A tensor.
+        x2: A tensor.
+
+    Returns:
+        tensor: Elementwise logaddexp.
+
     """
     return numpy.logaddexp(x1, x2)
 
@@ -94,12 +168,24 @@ def logcosh(x: T.Tensor) -> T.Tensor:
     """
     Elementwise logarithm of the hyperbolic cosine of a tensor.
 
+    Args:
+        x: A tensor.
+
+    Returns:
+        tensor: Elementwise logarithm of the hyperbolic cosine.
+
     """
     return -LOG2 + logaddexp(-x, x)
 
 def acosh(x: T.Tensor) -> T.Tensor:
     """
     Elementwise inverse hyperbolic cosine of a tensor.
+
+    Args:
+        x (greater than 1): A tensor.
+
+    Returns:
+        tensor: Elementwise inverse hyperbolic cosine.
 
     """
     y = numpy.clip(x,1+EPSILON, numpy.inf)
@@ -109,6 +195,12 @@ def logit(x: T.Tensor) -> T.Tensor:
     """
     Elementwise logit function of a tensor. Inverse of the expit function.
 
+    Args:
+        x (between 0 and 1): A tensor.
+
+    Returns:
+        tensor: Elementwise logit function
+
     """
     y = numpy.clip(x, a_min=EPSILON, a_max=1-EPSILON)
     return ne.evaluate('log(y/(1-y))')
@@ -117,6 +209,12 @@ def softplus(x: T.Tensor) -> T.Tensor:
     """
     Elementwise softplus function of a tensor.
 
+    Args:
+        x: A tensor.
+
+    Returns:
+        tensor: Elementwise softplus.
+
     """
     return numpy.logaddexp(0, x)
 
@@ -124,12 +222,24 @@ def cos(x: T.Tensor) -> T.Tensor:
     """
     Elementwise cosine of a tensor.
 
+    Args:
+        x: A tensor.
+
+    Returns:
+        tensor: Elementwise cosine.
+
     """
     return ne.evaluate('cos(x)')
 
 def sin(x: T.Tensor) -> T.Tensor:
     """
     Elementwise sine of a tensor.
+
+    Args:
+        x: A tensor.
+
+    Returns:
+        tensor: Elementwise sine.
 
     """
     return ne.evaluate('sin(x)')
