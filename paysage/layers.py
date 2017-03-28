847fd5b6f9567aeeebee1c39eed20927397e4f1f
Jon <jrwalsh1@gmail.com>
2017-03-28 01:43:54 -0700

    change formatting

--- paysage/layers.py
+++ paysage/layers.py
@@ -35,14 +35,15 @@ class Layer(object):
         Returns:
             A dictionary configuration for the layer.
         """
-        return {"layer_type": self.__class__.__name__,
-                "intrinsic": list(self.int_params.keys()),
-                "extrinsic": list(self.ext_params.keys()),
-                "penalties": {pk: self.penalties[pk].__name__
-                                for pk in self.penalties},
-                "constraints": {ck: self.constraints[ck].__name__
-                                for ck in self.constraints}
-               }
+        return {
+        "layer_type": self.__class__.__name__,
+        "intrinsic": list(self.int_params.keys()),
+        "extrinsic": list(self.ext_params.keys()),
+        "penalties": {pk: self.penalties[pk].__name__
+                        for pk in self.penalties},
+        "constraints": {ck: self.constraints[ck].__name__
+                        for ck in self.constraints}
+        }
 
     def get_config(self):
         """
