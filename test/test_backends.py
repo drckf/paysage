f2da166aec49fc66e1b193df037995f091be7ccf
Charles Fisher <charleskennethfisher@gmail.com>
2017-03-30 08:40:17 -0700

    add tests for multiply and divide functions

--- test/test_backends.py
+++ test/test_backends.py
@@ -1065,7 +1065,6 @@ def test_add():
     torch_add = torch_matrix.add(torch_a, torch_b)
     assert_close(py_add, torch_add, "add: (M, N) x (M, N)")
 
-
 def test_subtract():
     N = 100
     M = 50
@@ -1129,6 +1128,134 @@ def test_subtract():
     py_subtract = py_matrix.subtract(py_a, py_b)
     torch_subtract = torch_matrix.subtract(torch_a, torch_b)
     assert_close(py_subtract, torch_subtract, "subtract: (M, N) x (M, N)")
+
+def test_multiply():
+    N = 100
+    M = 50
+
+    # (N, 1) x (N, M)
+    a_shape = (N, 1)
+    b_shape = (N, M)
+
+    py_rand.set_seed()
+    py_a = py_rand.rand(a_shape)
+    py_b = py_rand.rand(b_shape)
+
+    torch_a = torch_matrix.float_tensor(py_a)
+    torch_b = torch_matrix.float_tensor(py_b)
+
+    py_multiply = py_matrix.multiply(py_a, py_b)
+    torch_multiply = torch_matrix.multiply(torch_a, torch_b)
+    assert_close(py_multiply, torch_multiply, "multiply: (N, 1) x (N, M)")
+
+    # (1, N) x (M, N)
+    a_shape = (1, N)
+    b_shape = (M, N)
+
+    py_rand.set_seed()
+    py_a = py_rand.rand(a_shape)
+    py_b = py_rand.rand(b_shape)
+
+    torch_a = torch_matrix.float_tensor(py_a)
+    torch_b = torch_matrix.float_tensor(py_b)
+
+    py_multiply = py_matrix.multiply(py_a, py_b)
+    torch_multiply = torch_matrix.multiply(torch_a, torch_b)
+    assert_close(py_multiply, torch_multiply, "multiply: (1, N) x (M, N)")
+
+    # (N,) x (M, N)
+    a_shape = (N,)
+    b_shape = (M, N)
+
+    py_rand.set_seed()
+    py_a = py_rand.rand(a_shape)
+    py_b = py_rand.rand(b_shape)
+
+    torch_a = torch_matrix.float_tensor(py_a)
+    torch_b = torch_matrix.float_tensor(py_b)
+
+    py_multiply = py_matrix.multiply(py_a, py_b)
+    torch_multiply = torch_matrix.multiply(torch_a, torch_b)
+    assert_close(py_multiply, torch_multiply, "multiply: (N,) x (M, N)")
+
+    # (M, N) x (M, N)
+    a_shape = (M, N)
+    b_shape = (M, N)
+
+    py_rand.set_seed()
+    py_a = py_rand.rand(a_shape)
+    py_b = py_rand.rand(b_shape)
+
+    torch_a = torch_matrix.float_tensor(py_a)
+    torch_b = torch_matrix.float_tensor(py_b)
+
+    py_multiply = py_matrix.multiply(py_a, py_b)
+    torch_multiply = torch_matrix.multiply(torch_a, torch_b)
+    assert_close(py_multiply, torch_multiply, "multiply: (M, N) x (M, N)")
+
+def test_divide():
+    N = 100
+    M = 50
+
+    # (N, 1) x (N, M)
+    a_shape = (N, 1)
+    b_shape = (N, M)
+
+    py_rand.set_seed()
+    py_a = py_rand.rand(a_shape)
+    py_b = py_rand.rand(b_shape)
+
+    torch_a = torch_matrix.float_tensor(py_a)
+    torch_b = torch_matrix.float_tensor(py_b)
+
+    py_divide = py_matrix.divide(py_a, py_b)
+    torch_divide = torch_matrix.divide(torch_a, torch_b)
+    assert_close(py_divide, torch_divide, "divide: (N, 1) x (N, M)")
+
+    # (1, N) x (M, N)
+    a_shape = (1, N)
+    b_shape = (M, N)
+
+    py_rand.set_seed()
+    py_a = py_rand.rand(a_shape)
+    py_b = py_rand.rand(b_shape)
+
+    torch_a = torch_matrix.float_tensor(py_a)
+    torch_b = torch_matrix.float_tensor(py_b)
+
+    py_divide = py_matrix.divide(py_a, py_b)
+    torch_divide = torch_matrix.divide(torch_a, torch_b)
+    assert_close(py_divide, torch_divide, "divide: (1, N) x (M, N)")
+
+    # (N,) x (M, N)
+    a_shape = (N,)
+    b_shape = (M, N)
+
+    py_rand.set_seed()
+    py_a = py_rand.rand(a_shape)
+    py_b = py_rand.rand(b_shape)
+
+    torch_a = torch_matrix.float_tensor(py_a)
+    torch_b = torch_matrix.float_tensor(py_b)
+
+    py_divide = py_matrix.divide(py_a, py_b)
+    torch_divide = torch_matrix.divide(torch_a, torch_b)
+    assert_close(py_divide, torch_divide, "divide: (N,) x (M, N)")
+
+    # (M, N) x (M, N)
+    a_shape = (M, N)
+    b_shape = (M, N)
+
+    py_rand.set_seed()
+    py_a = py_rand.rand(a_shape)
+    py_b = py_rand.rand(b_shape)
+
+    torch_a = torch_matrix.float_tensor(py_a)
+    torch_b = torch_matrix.float_tensor(py_b)
+
+    py_divide = py_matrix.divide(py_a, py_b)
+    torch_divide = torch_matrix.divide(torch_a, torch_b)
+    assert_close(py_divide, torch_divide, "divide: (M, N) x (M, N)")
 
 def test_affine():
     # vector-vector-matrix
