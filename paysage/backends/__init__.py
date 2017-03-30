5ff601ef9f4f1b862b89adce23ff5d07e001bbab
Charles Fisher <charleskennethfisher@gmail.com>
2017-03-30 08:59:31 -0700

    add namedtuple functions to backend

--- paysage/backends/__init__.py
+++ paysage/backends/__init__.py
@@ -21,3 +21,63 @@ else:
     "Unknown backend {}".format(config['backend'])
     )
 
+
+# ----- COMMON FUNCTIONALITY ----- #
+
+from collections import namedtuple
+
+def add_tuples(a, b):
+    """
+    Add tuple a to tuple b entrywise.
+
+    Args:
+        a (namedtuple): (key: tensor)
+        b (namedtuple): (key: tensor)
+
+    Returns:
+        namedtuple: a + b (key: tensor)
+
+    """
+    return type(a)(*(add(x[0], x[1]) for x in zip(a, b)))
+
+def subtract_tuples(a, b):
+    """
+    Subtract tuple a from tuple b entrywise.
+
+    Args:
+        a (namedtuple): (key: tensor)
+        b (namedtuple): (key: tensor)
+
+    Returns:
+        namedtuple: b - a (key: tensor)
+
+    """
+    return type(a)(*(subtract(x[0], x[1]) for x in zip(a, b)))
+
+def multiply_tuples(a, b):
+    """
+    Multiply tuple b by tuple a entrywise.
+
+    Args:
+        a (namedtuple): (key: tensor)
+        b (namedtuple): (key: tensor)
+
+    Returns:
+        namedtuple: a * b (key: tensor)
+
+    """
+    return type(a)(*(multiply(x[0], x[1]) for x in zip(a, b)))
+
+def divide_tuples(a, b):
+    """
+    Divide tuple b by tuple a entrywise.
+
+    Args:
+        a (namedtuple; non-zero): (key: tensor)
+        b (namedtuple): (key: tensor)
+
+    Returns:
+        namedtuple: b / a (key: tensor)
+
+    """
+    return type(a)(*(divide(x[0], x[1]) for x in zip(a, b)))
