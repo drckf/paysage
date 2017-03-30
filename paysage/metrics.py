89f598bb0cc9d32e0caa38eb24d14abbd8cf239a
Charles Fisher <charleskennethfisher@gmail.com>
2017-03-29 18:02:14 -0700

    EnergyZscore docstrings

--- paysage/metrics.py
+++ paysage/metrics.py
@@ -213,7 +213,7 @@ class EnergyGap(object):
     #TODO: use State objects instead of tensors
     def update(self, minibatch=None, random_samples=None, amodel=None, **kwargs):
         """
-        Update the estimate for the energy distance using a batch
+        Update the estimate for the energy gap using a batch
         of observations and a batch of fantasy particles.
 
         Notes:
@@ -253,26 +253,87 @@ class EnergyGap(object):
 
 
 class EnergyZscore(object):
+    """
+    Samples drawn from a model should have much lower energy
+    than purely random samples. The "energy gap" is the average
+    energy difference between samples from the model and random
+    samples. The "energy z-score" is the energy gap divided by
+    the standard deviation of the energy taken over random
+    samples.
+
+    """
 
     name = 'EnergyZscore'
 
     def __init__(self):
+        """
+        Create an EnergyZscore object.
+
+        Args:
+            None
+
+        Returns:
+            energy z-score object
+
+        """
         self.data_mean = 0
         self.random_mean = 0
         self.random_mean_square = 0
 
     def reset(self):
+        """
+        Reset the metric to it's initial state.
+
+        Note:
+            Modifies norm, random_mean, and random_mean_square in place.
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        """
         self.data_mean = 0
         self.random_mean = 0
         self.random_mean_square = 0
 
     #TODO: use State objects instead of tensors
     def update(self, minibatch=None, random_samples=None, amodel=None, **kwargs):
+        """
+        Update the estimate for the energy z-score using a batch
+        of observations and a batch of fantasy particles.
+
+        Notes:
+            Changes norm, random_mean, and random_mean_square in place.
+
+        Args:
+            minibatch (tensor (num_samples, num_units)):
+                samples from the model
+            random_samples (tensor (num_samples, num))
+            amodel (Model): the model
+            kwargs: key word arguments
+                not used, but helpful for looping through metric functions
+
+        Returns:
+            None
+
+        """
         self.data_mean += be.mean(amodel.marginal_free_energy(minibatch))
         self.random_mean +=  be.mean(amodel.marginal_free_energy(random_samples))
         self.random_mean_square +=  be.mean(amodel.marginal_free_energy(random_samples)**2)
 
     def value(self):
+        """
+        Get the value of the energy z-score.
+
+        Args:
+            None
+
+        Returns:
+            energy z-score (float)
+
+        """
         if self.random_mean_square:
             return (self.data_mean - self.random_mean) / math.sqrt(self.random_mean_square)
         else:
