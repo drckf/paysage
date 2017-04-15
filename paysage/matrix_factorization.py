2445f32fbccd38a8525730962c0591892b692ed3
Evan Kriminger <evankriminger@gmail.com>
2017-04-15 03:19:48 -0400

    pca working, need to fix kmeans

--- paysage/matrix_factorization.py
+++ paysage/matrix_factorization.py
@@ -3,8 +3,10 @@ import numpy as np
 class KMeans(object):
     """Class which implements kmeans clustering a la 
     Lloyd's algorithm
+ 
+    TODO: fix convergence issues, prevent empty clusters
     """
-    def __init__(self, data, k):
+    def __init__(self, k):
         self.k = k
         
     def fit(self, data, max_it=50, num_attempts=3):
@@ -18,21 +20,24 @@ class KMeans(object):
         final_fit = np.zeros((self.k, self.data.shape[1]), dtype=self.data.dtype)
         for i in range(num_attempts):
             self._initialize()
-            res = self.compute_kmeans(max_it)
+            res = self._compute_kmeans(max_it)
             if res:
-                final_result += self.centers
+                final_fit += self.centers
                 num_success += 1
+        self.centers = final_fit/num_success
+        self._assign()       
 
-    def compute_kmeans(self, max_it):
+    def _compute_kmeans(self, max_it):
         """A single run of the kmeans algorithm
         """
         i_it, converged = 0, False
         while not converged and i_it < max_it:
             old_ids = self.cluster_id
-            self.assign()
+            print(self.centers)
+            self._assign()
             if np.all(old_ids==self.cluster_id):
                 converged = True
-            self.update()        
+            self._update()        
             i_it += 1
 
         return converged
@@ -43,29 +48,29 @@ class KMeans(object):
         assert self.k < self.N, "Must have more samples than clusters"
         center_ind = np.random.choice(self.N, self.k)
         self.centers = self.data[center_ind,:]
-        self.cluster_id = np.zeros((self,N,), dtype=int)
+        self._assign()        
 
-    def assign(self):
+    def _assign(self):
         """Assign step, in which we find the closest center to each
         sample
         """
-        self.cluster_id = np.argmin(np.sum((self.data[:,:,None] - self.centers[:,:,None].reshape((1,-1,self.k)))**2, axis=1),)
+        self.cluster_id = np.argmin(np.sum((self.data[:,:,None] - self.centers[:,:,None].reshape((1,-1,self.k)))**2, axis=1), axis=1)
 
-    def update(self):
+    def _update(self):
         """Update step, in which we compute the new cluster centers
         given the updated cluster assignments
         """
         for i in range(self.k):
             self.centers[i,:] = np.mean(self.data[self.cluster_id==i,:], axis=0)
+       
     
-        
-    
-
 class PCA(object):
     def __init__(self, k):
         self.k = k
        
     def fit(self, data):
+        assert data.shape[1] > self.k, "must be more initial dimensions \
+                                        than the final mapping"
         N = data.shape[0]
         data_zm = data - np.mean(data, axis=0)[None,:]
         cov = np.dot(data_zm, data_zm.transpose())/(N-1)
@@ -77,3 +82,21 @@ class PCA(object):
 def archetype(data):
     pass
 
+
+if __name__=='__main__':
+    import matplotlib.pyplot as plt
+   
+    data = np.concatenate((np.random.randn(50,2), 10+np.random.randn(50,2)), axis=0)
+   
+    method = 'pca'
+    if method=='kmeans': 
+        kmeans = KMeans(2)
+        kmeans.fit(data)
+        plt.scatter(data[:,0], data[:,1])
+        plt.plot(kmeans.centers[:,0], kmeans.centers[:,1], 'r.')
+        plt.show()
+    elif method=='pca':
+        pca = PCA(1)
+        new_data = pca.fit(data)
+        plt.scatter(new_data, np.ones((new_data.size,)))
+        plt.show()
