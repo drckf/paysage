98fe9627dcdaf2530dc734fcd52f1e23204d2a2a
Evan Kriminger <evankriminger@gmail.com>
2017-04-15 19:00:46 -0400

    added some formatting

--- paysage/matrix_factorization.py
+++ paysage/matrix_factorization.py
@@ -1,14 +1,37 @@
 import numpy as np
 
 class KMeans(object):
-    """Class which implements kmeans clustering a la Lloyd's algorithm
-    """
+    """Class which implements kmeans clustering with Lloyd's algorithm"""
+
     def __init__(self, k):
+        """
+        Specify the number of clusters, which corresponds to the number of 
+        hidden layers if this is being used as a weight initialization.
+        
+        Args:
+            k (int): number of clusters
+    
+        Returns:
+            KMeans object
+        """
         self.k = k
         
     def fit(self, data, max_it=50, num_attempts=3):
-        """Main call to compute a kmeans clustering of the data. Averages
-        multiple successful runs to achieve a better result.
+        """
+        Main call to compute a kmeans clustering of the data. Uses the best of 
+        multiple runs to achieve a better result. Cluster labels are stored in 
+        the cluster_id attribute, while 
+
+        Args:
+            data (tensor (num_samples, num_dim)):
+                Data matrix to cluster
+            max_it (int, optional): 
+                Maximum number of iterations alotted
+            num_attempts (int, optional): 
+                Number of times to run kmeans for finding better solution
+
+        Returns:
+            None
         """
         self.data = data
         self.N = self.data.shape[0]
@@ -26,7 +49,8 @@ class KMeans(object):
         self._assign()       
 
     def _compute_kmeans(self, max_it):
-        """A single run of the kmeans algorithm
+        """
+        A single run of the kmeans algorithm
         """
         i_it, converged = 0, False
         while not converged and i_it < max_it: 
@@ -40,7 +64,8 @@ class KMeans(object):
         return converged
 
     def _initialize(self):
-        """Initialization using the Forgy method
+        """
+        Initialization using the Forgy method
         """
         assert self.k < self.N, "Must have more samples than clusters"
         self.cluster_id = np.random.randint(0, self.k, (self.N,))
@@ -48,29 +73,51 @@ class KMeans(object):
         self.centers = self.data[center_ind,:]
 
     def _assign(self):
-        """Assign step, in which we find the closest center to each sample
+        """
+        Assign step, in which we find the closest center to each sample
         """
         diff = self.data[:,:,None] - self.centers[:,:,None].transpose((2,1,0))
         self.cluster_id = np.argmin(np.sum(diff**2, axis=1), axis=1)
 
     def _update(self):
-        """Update step, in which we compute the new cluster centers given the 
+        """
+        Update step, in which we compute the new cluster centers given the 
         updated cluster assignments
         """
         for i in range(self.k):
             self.centers[i,:] = np.mean(self.data[self.cluster_id==i,:], axis=0)
 
     def _compute_loss(self):
-        """Compute the squared intra-cluster L2 norm
+        """
+        Compute the squared intra-cluster L2 norm
         """
         return np.sum((self.data - self.centers[self.cluster_id,:])**2)
     
 
 class PCA(object):
+    """Class which implements PCA"""
+
     def __init__(self, k):
+        """
+        Initialize visible to hidden weights using a PCA projection
+         
+        Args:
+            k (int): number of hidden layers
+           
+        Returns:
+            PCA object
+        """
         self.k = k
        
     def fit(self, data):
+        """
+        Args:
+            data (tensor (num_samples, num_dim)): 
+                Input data matrix.
+
+        Returns:
+            tensor (num_visible, num_hidden): initialization for new weights
+        """
         assert data.shape[1] > self.k, "must be more initial dimensions than \
                                         the final mapping"
         N = data.shape[0]
