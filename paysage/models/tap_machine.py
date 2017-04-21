554a2bff60820efb49ae777b02bf4bd9a71603e2
d-rams <geminatea@gmail.com>
2017-04-20 23:54:23 -0700

    A little reorganization and an option for 1,2,3 terms in the tap expansion

--- paysage/models/tap_machine.py
+++ paysage/models/tap_machine.py
@@ -25,7 +25,7 @@ class TAP_rbm(model.Model):
 
     """
 
-    def __init__(self, layer_list, init_lr_EMF=0.1, tolerance_EMF=1e-2, max_iters_EMF=100, num_persistent_samples=0):
+    def __init__(self, layer_list, terms=2, init_lr_EMF=0.1, tolerance_EMF=1e-2, max_iters_EMF=100, num_persistent_samples=0):
         """
         Create a TAP rbm model.
 
@@ -34,6 +34,7 @@ class TAP_rbm(model.Model):
 
         Args:
             layer_list: A list of layers objects.
+            terms: number of terms to use in the TAP expansion (#TODO: deprecate this attribute when we turn tap training into a method and use tap1,tap2,tap3 as methods)
 
             EMF computation parameters:
                 init_lr float: initial learning rate which is halved whenever necessary to enforce descent.
@@ -55,8 +56,12 @@ class TAP_rbm(model.Model):
             self.persistent_samples.append(None)
         self.tap_seed = None
 
+        if terms != 1 and terms != 2 and terms != 3:
+            raise ValueError("Must specify one, two, or three terms in TAP expansion training method")
+        self.terms = terms
 
-    def gibbs_free_energy_TAP3(self, seed=None, init_lr=0.1, tol=1e-4, max_iters=500):
+
+    def gibbs_free_energy(self, seed=None, init_lr=0.1, tol=1e-4, max_iters=50, terms=2):
         """
         Compute the Gibbs free engergy of the model according to the TAP
         expansion around infinite temperature to second order.
@@ -87,67 +92,91 @@ class TAP_rbm(model.Model):
                                           Chosing 'None' will result in a random seed
             init_lr float: initial learning rate which is halved whenever necessary to enforce descent.
             tol float: tolerance for quitting minimization.
-            max_iters: maximum gradient decsent steps
+            max_iters: maximum gradient decsent steps.
+            terms: number of terms to use (1, 2, or 3 allowed)
 
         Returns:
             tuple (magnetization, TAP3-approximated Gibbs free energy)
                   (Magnetization, float)
 
         """
+        if terms != 1 and terms != 2 and terms != 3:
+            raise ValueError("Must specify one, two, or three terms in TAP expansion training method")
 
         w = self.weights[0].int_params.matrix
         a = self.layers[0].int_params.loc
         b = self.layers[1].int_params.loc
 
+        # Mean field derivatives
+        def grad_v_gamma_MF(m, w, a):
+            return be.log(be.divide(1.0 - m.v, m.v)) - a - be.dot(w, m.h)
+        def grad_h_gamma_MF(m, w, b):
+            return be.log(be.divide(1.0 - m.h, m.h)) - b - be.dot(m.v,w)
+
+        # MF plus Onsager reaction term
         def grad_v_gamma_TAP2(m, w, a):
             m_h_quad = m.h - be.square(m.h)
             ww = be.square(w)
             return be.log(be.divide(1.0 - m.v, m.v)) - a - be.dot(w, m.h) - \
                    be.multiply(0.5 - m.v,be.dot(ww, m_h_quad))
-
         def grad_h_gamma_TAP2(m, w, b):
             m_v_quad = m.v - be.square(m.v)
             ww = be.square(w)
             return be.log(be.divide(1.0 - m.h, m.h)) - b - be.dot(m.v,w) - \
                    be.multiply(be.dot(m_v_quad,ww),0.5 - m.h)
 
+        # Third order TAP expansion gradients
         def grad_v_gamma_TAP3(m, w, a):
             m_h_quad = m.h - be.square(m.h)
             ww = be.square(w)
             www = be.multiply(ww,w)
             return be.log(be.divide(1.0 - m.v, m.v)) - a - be.dot(w, m.h) - \
                    be.multiply(0.5 - m.v,be.dot(ww, m_h_quad)) - \
-                   (4.0/3.0)*be.multiply(0.5 - 3.0*m.v + 3.0*be.square(m.v), be.dot(www,be.multiply(0.5 - m.h, m_h_quad)))
-
+                   (4.0/3.0)*be.multiply(0.5 - 3.0*m.v + 3.0*be.square(m.v),\
+                   be.dot(www,be.multiply(0.5 - m.h, m_h_quad)))
         def grad_h_gamma_TAP3(m, w, b):
             m_v_quad = m.v - be.square(m.v)
             ww = be.square(w)
             www = be.multiply(ww,w)
             return be.log(be.divide(1.0 - m.h, m.h)) - b - be.dot(m.v,w) - \
                    be.multiply(be.dot(m_v_quad,ww),0.5 - m.h) - \
-                   (4.0/3.0)*be.multiply(be.dot(be.multiply(0.5 - m.v, m_v_quad),www),0.5 - 3.0*m.h + 3.0*be.square(m.h))
+                   (4.0/3.0)*be.multiply(be.dot(be.multiply(0.5 - m.v, m_v_quad),www),\
+                   0.5 - 3.0*m.h + 3.0*be.square(m.h))
 
-        def minimize_gamma_GD(w, a, b, m, init_lr, tol, max_iters):
+        def minimize_gamma_GD(w, a, b, m, init_lr, tol, max_iters, terms):
             """
             Simple gradient descent routine to minimize Gamma
 
             Warning: this function modifies seed to avoid an extra copy and allocation
 
             """
+            if terms == 1:
+                gamma = self.gamma_MF
+                grad_v_gamma = grad_v_gamma_MF
+                grad_h_gamma = grad_h_gamma_MF
+            elif terms == 2:
+                gamma = self.gamma_TAP2
+                grad_v_gamma = grad_v_gamma_TAP2
+                grad_h_gamma = grad_h_gamma_TAP2
+            elif terms == 3:
+                gamma = self.gamma_MF
+                grad_v_gamma = grad_v_gamma_TAP3
+                grad_h_gamma = grad_h_gamma_TAP3
+
             eps = 1e-6
             its = 0
             lr = init_lr
-            gam = self.gamma_TAP3(m, w, a, b)
+            gam = gamma(m, w, a, b)
 
             while (its < max_iters):
                 its += 1
-                m_provisional = Magnetization(m.v - lr*grad_v_gamma_TAP3(m, w, a),
-                                              m.h - lr*grad_h_gamma_TAP3(m, w, b))
+                m_provisional = Magnetization(m.v - lr*grad_v_gamma(m, w, a),
+                                              m.h - lr*grad_h_gamma(m, w, b))
                 # Warning: in general a lot of clipping gets done here
                 be.clip_inplace(m_provisional.v, eps, 1.0-eps)
                 be.clip_inplace(m_provisional.h, eps, 1.0-eps)
 
-                gam_provisional = self.gamma_TAP3(m_provisional, w, a, b)
+                gam_provisional = gamma(m_provisional, w, a, b)
                 if (gam - gam_provisional < 0):
                     lr *= 0.5
                     if (lr < 1e-10):
@@ -199,8 +228,16 @@ class TAP_rbm(model.Model):
                 0.99 * be.float_tensor(be.rand((num_hidden_units,))) + 0.005
             )
 
-        return minimize_gamma_GD(w, a, b, seed, init_lr, tol, max_iters)
+        return minimize_gamma_GD(w, a, b, seed, init_lr, tol, max_iters, terms)
         #return minimize_gamma_constraint_sat(w, a, b, seed, tol, max_iters)
+
+    # The Legendre transform of F(v;q) as a function of q according to Mean Field approximation
+    # specialized to the RBM case
+    def gamma_MF(self, m, w, a, b):
+        return \
+            be.tsum(be.multiply(m.v, be.log(m.v)) + be.multiply(1.0 - m.v, be.log(1.0 - m.v))) + \
+            be.tsum(be.multiply(m.h, be.log(m.h)) + be.multiply(1.0 - m.h, be.log(1.0 - m.h))) - \
+            be.dot(a,m.v) - be.dot(b,m.h) - be.dot(m.v, a + be.dot(w,m.h))
 
     # The Legendre transform of F(v;q) as a function of q according to TAP expansion 2 terms
     # specialized to the RBM case
@@ -239,6 +276,31 @@ class TAP_rbm(model.Model):
         """
         return -be.dot(a,v) - be.tsum(be.logaddexp((b + be.dot(v,w)), be.zeros_like(b)))
 
+    def grad_a_gamma_MF(self, m, w, a, b):
+        return -m.v
+    def grad_b_gamma_MF(self, m, w, a, b):
+        return -m.h
+    def grad_w_gamma_MF(self, m, w, a, b):
+        return -be.outer(m.v, m.h)
+
+    def grad_a_gamma_TAP2(self, m, w, a, b):
+        return -m.v
+    def grad_b_gamma_TAP2(self, m, w, a, b):
+        return -m.h
+    def grad_w_gamma_TAP2(self, m, w, a, b):
+        m_v_quad = m.v - be.square(m.v)
+        m_h_quad = m.h - be.square(m.h)
+        return -be.outer(m.v, m.h) - be.multiply(w, be.outer(m_v_quad, m_h_quad))
+
+    def grad_a_gamma_TAP3(self, m, w, a, b):
+        return -m.v
+    def grad_b_gamma_TAP3(self, m, w, a, b):
+        return -m.h
+    def grad_w_gamma_TAP3(self, m, w, a, b):
+        m_v_quad = m.v - be.square(m.v)
+        m_h_quad = m.h - be.square(m.h)
+        return -be.outer(m.v, m.h) - be.multiply(w, be.outer(m_v_quad, m_h_quad))
+
     def gradient(self, vdata, vmodel):
         """
         Gradient of -\ln P(v) with respect to the weights and biases
@@ -249,32 +311,44 @@ class TAP_rbm(model.Model):
         a = self.layers[0].int_params.loc
         b = self.layers[1].int_params.loc
 
+        if self.terms == 1:
+             grad_a_gamma = self.grad_a_gamma_MF
+             grad_b_gamma = self.grad_b_gamma_MF
+             grad_w_gamma = self.grad_w_gamma_MF
+        elif self.terms == 2:
+             grad_a_gamma = self.grad_a_gamma_TAP2
+             grad_b_gamma = self.grad_b_gamma_TAP2
+             grad_w_gamma = self.grad_w_gamma_TAP2
+        elif self.terms == 3:
+             grad_a_gamma = self.grad_a_gamma_TAP3
+             grad_b_gamma = self.grad_b_gamma_TAP3
+             grad_w_gamma = self.grad_w_gamma_TAP3
+
         # compute the TAP3 approximation to the Gibbs free energy:
         EMF = 1e6
         m = None
         if len(self.persistent_samples) == 0: # random seed
-                (m,EMF) = self.gibbs_free_energy_TAP3(m,
-                                                      self.init_lr_EMF,
-                                                      self.tolerance_EMF,
-                                                      self.max_iters_EMF)
+                (m,EMF) = self.gibbs_free_energy(m,
+                                                 self.init_lr_EMF,
+                                                 self.tolerance_EMF,
+                                                 self.max_iters_EMF,
+                                                 self.terms)
         else:
             best_EMF = 1e7
             for s in range(len(self.persistent_samples)): # persistent seeds
-                (self.persistent_samples[s],EMF) = self.gibbs_free_energy_TAP3(self.persistent_samples[s],
-                                                                               self.init_lr_EMF,
-                                                                               self.tolerance_EMF,
-                                                                               self.max_iters_EMF)
+                (self.persistent_samples[s],EMF) = self.gibbs_free_energy(self.persistent_samples[s],
+                                                                          self.init_lr_EMF,
+                                                                          self.tolerance_EMF,
+                                                                          self.max_iters_EMF,
+                                                                          self.terms)
                 if EMF < best_EMF:
                     best_EMF = EMF
                     m = self.persistent_samples[s]
 
         # Compute the gradients at this minimizing magnetization
-        m_v_quad = m.v - be.square(m.v)
-        m_h_quad = m.h - be.square(m.h)
-
-        dw_EMF = -be.outer(m.v, m.h) - be.multiply(w, be.outer(m_v_quad, m_h_quad))
-        da_EMF = -m.v
-        db_EMF = -m.h
+        dw_EMF = grad_w_gamma(m,w,a,b)
+        da_EMF = grad_a_gamma(m,w,a,b)
+        db_EMF = grad_b_gamma(m,w,a,b)
 
         # compute average grad_F_marginal over the minibatch
         intermediate = be.expit(be.dot(vdata,w) + b)
@@ -282,8 +356,6 @@ class TAP_rbm(model.Model):
         da = be.mean(vdata, axis=0)
         db = be.mean(intermediate, axis=0)
         batch_size = be.shape(vdata)[0]
-        # This is the same as \sum_{i} vdata[i] \outer intermediate[i]
-        # TODO: is this efficient?
         dw = be.dot(be.transpose(vdata), intermediate) / batch_size
 
         grad = gu.Gradient(
