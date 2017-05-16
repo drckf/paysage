841423ba091c9e7bfcd49d7379fc2a83ac74318d
d-rams <geminatea@gmail.com>
2017-05-15 17:13:22 -0700

    rm tap_machine from models module

--- paysage/models/__init__.py
+++ paysage/models/__init__.py
@@ -2,6 +2,5 @@ from .initialize import init_model
 from .initialize import init_recurrent
 from .initialize import init_deep
 from . import model
-from . import tap_machine
 from . import recurrent
 from . import deep
