c57d1e474f657d1f3c95039d4580bc46369de4d9
Jon <jrwalsh1@gmail.com>
2017-05-17 12:06:07 -0700

    adding some graph stuff

--- paysage/models/model_utils.py
+++ paysage/models/model_utils.py
@@ -193,6 +193,184 @@ class WeightConnection(object):
         self.trainable = trainable
 
 
+class IncidenceMatrix(object):
+    """
+    Holds the incidence matrix for a graph.
+    Allows operations to extract the adjacency matrix, adjacency list, and edge list.
+
+    """
+    def __init__(self, incidence_matrix):
+        self.incidence_matrix = None
+        self.edge_list = None
+        self.adjacency_matrix = None
+        self.adjacency_list = None
+
+        # update the other graph attributes
+        self.update(incidence_matrix)
+
+    def update(self, incidence_matrix):
+        """
+        Update the incidence_matrix, edge list,
+            adjacency matrix, adjacency list, and edge list attributes.
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        """
+        self.incidence_matrix = incidence_matrix
+        self.edge_list = self._edge_list()
+        self.adjacency_matrix = self._adjacency_matrix()
+        self.adjacency_list = self._adjacency_list()
+
+    def _edge_list(self):
+        """
+        Get the edge list.
+        Needs a current incidence matrix.
+
+        Args:
+            None
+
+        Returns:
+            edge_list (List): the list of edges in the graph.
+
+        """
+        return [np.nonzero(col)[0] for col in self.incidence_matrix.T]
+
+    def _adjacency_matrix(self):
+        """
+        Get the adjacency matrix.
+        Needs a current incidence matrix and edge list.
+
+        Args:
+            None
+
+        Returns:
+            adjacency_matrix (numpy array): the adjacency matrix
+
+        """
+        adj = np.zeros(2*[len(self.incidence_matrix)])
+        for edge in self.edge_list:
+            adj[edge, edge[::-1]] = 1
+        return adj
+
+    def _adjacency_list(self):
+        """
+        Get the adjacency list.
+        Needs a current adjacency matrix.
+
+        Args:
+            None
+
+        Returns:
+            adjacency_list (List): the adjacency list
+        """
+        return [np.nonzero(row)[0] for row in self.adjacency_matrix]
+
+
+class ComputationGraph(object):
+    """
+    Manages the connections between layers in a model.
+    Layers can have various properties set:
+        - Clamped sampling: the layer is not sampled (state unchanged)
+        - Trainable: the layer's parameters can be changed
+        - Excluded: the layer is not used in the model
+            (its connections are circumvented)
+    Weight layers (connecting two other layers) can have
+        the trainable and excluded properties set.
+
+    The computation graph is defined by an IncidenceMatrix, where
+    layers are vertices and weight layers are edges.
+    The incidence matrix defines the connections between layers
+    and the corresponding edges.
+
+    """
+    def __init__(self, num_layers):
+        """
+        Builds the default incidence matrix.
+
+        Args:
+            num_layers: the number of layers in the model
+
+        Returns:
+            None
+
+        """
+        self.num_layers = num_layers
+
+        # build the incidence matrix
+        incidence_matrix = self.default_incidence_matrix()
+        self.connections = IncidenceMatrix(incidence_matrix)
+
+        # define the connections between layers
+        self.layer_connections = self.connections.adjacency_list
+        self.weight_connections = self.connections.edge_list
+
+        # the default properties
+        # all layers can be sampled, trained, not excluded
+        # all weights can be trained, not excluded
+        self.clamped_sampling = []
+        self.trainable_layers = range(len(self.layer_connections))
+        self.trainable_weights = range(len(self.weight_connections))
+        self.excluded_layers = []
+        self.excluded_weights = []
+
+
+    def default_incidence_matrix(self):
+        """
+        Builds the default incidence matrix.
+
+        Args:
+            None
+
+        Returns:
+            incidence_matrix: a matrix specifying the connections in the model.
+
+        """
+        incidence_matrix = np.zeros((self.num_layers, self.num_layers-1))
+        for i in range(self.num_layers-1):
+            incidence_matrix[i, i] = 1
+            incidence_matrix[i+1, i] = 1
+        return incidence_matrix
+
+
+    def exclude_layer(self, incidence_matrix, excluded_layer):
+        """
+        Returns an incidence matrix from a given one,
+            excluding a single layer.
+        Excluding a layer does the following:
+            - any edges (weights) connected to layer where it has the higher index
+                are excluded
+            - any edges (weights) connected to the layer where it has the lower index
+                transfer their connection to the next lowest index layer that
+                the layer is connected to.
+
+        Example: 4 layers, 3 weights case
+                 W_0 W_1 W_2
+            L_0:  1   0   0
+            L_1:  1   1   0
+            L_2:  0   1   1
+            L_3:  0   0   1
+
+        exclude layer 2:
+            - W_1 excluded (L_2 is the higher index vertex)
+            - W_2 transfers its connection to L_1 (L_2 was connected to L_1 and L_3).
+        result:
+                 W_0 W_1 W_2
+            L_0:  1   0   0
+            L_1:  1   0   1
+            L_2:  0   0   0
+            L_3:  0   0   1
+
+
+        """
+
+
+
+
+
 class ComputationGraph(object):
     """
     Manages the connections between layers in a model.
@@ -227,6 +405,37 @@ class ComputationGraph(object):
         self.weight_connections = self.build_weight_connections()
         # the layer connections are built from the weight_connections attribute
         self.layer_connections = self.build_layer_connections()
+
+    def print_graph(self):
+        """
+        Prints the state of the compute graph.  Used for debugging.
+        """
+        print("*** compute graph ***")
+        print("~~~~~~ layers ~~~~~~")
+        for i, lc in enumerate(self.layer_connections):
+            print("LAYER {}".format(i))
+            print("left connected layers:", lc.left_connected_layers)
+            print("left connected weights:", lc.left_connected_weights)
+            print("right connected layers:", lc.right_connected_layers)
+            print("right connected weights:", lc.right_connected_weights)
+            print("sampling clamped?", lc.sampling_clamped)
+            print("trainable?", lc.trainable)
+            print("excluded?", lc.excluded)
+
+        print("~~~~~~ weights ~~~~~~")
+        for i, wc in enumerate(self.weight_connections):
+            print("WEIGHT {}".format(i))
+            print("left connected layer:", wc.left_layer)
+            print("right connected layer:", wc.right_layer)
+            print("trainable?", wc.trainable)
+        print("*** _____________ ***")
+
+    def build_default_graph(self):
+        """
+
+        """
+
+
 
     def build_weight_connections(self, excluded=[]):
         """
@@ -274,6 +483,36 @@ class ComputationGraph(object):
         layer_connections = []
         for layer_index in range(self.num_layers):
             lc = LayerConnection()
+            # loop over all weights
+            for weight_index, weight in enumerate(self.weight_connections):
+                # if the layer is connected on the shallower side
+                if weight.left_layer == layer_index:
+                    lc.right_connected_layers.append(weight.right_layer)
+                    lc.right_connected_weights.append(weight_index)
+                # or if it's connected on the deeper side
+                elif weight.right_layer == layer_index:
+                    lc.left_connected_layers.append(weight.left_layer)
+                    lc.left_connected_weights.append(weight_index)
+            layer_connections.append(lc)
+        return layer_connections
+
+    def rebuild_layer_connections(self):
+        """
+        Rebuilds layer connections from the `weight_connections` attribute.
+
+        Args:
+            None
+
+        Returns:
+            layer_connections: a list of LayerConnection objects
+
+        """
+        layer_connections = []
+        for layer_index, lc in enumerate(self.layer_connections):
+            lc.left_connected_layers = []
+            lc.right_connected_layers = []
+            lc.left_connected_weights = []
+            lc.right_connected_weights = []
             # loop over all weights
             for weight_index, weight in enumerate(self.weight_connections):
                 # if the layer is connected on the shallower side
@@ -340,4 +579,4 @@ class ComputationGraph(object):
 
         # build the connections
         self.weight_connections = self.build_weight_connections(excluded=excluded_layers)
-        self.layer_connections = self.build_layer_connections()
+        self.layer_connections = self.rebuild_layer_connections()
